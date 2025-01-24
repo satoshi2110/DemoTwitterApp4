dcfb5c6c34b14329f7dffd9e17fd57a75b79b99a
satoshi2110 <zappawater@gmail.com>
2025-01-24 19:31:57 +0900

    tableViewに文字列を表示

---
+++ DemoTwitterApp4/TweetDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  TweetDataModel.swift
+//  DemoTwitterApp4
+//
+//  Created by N S on 2025/01/24.
+//
+
+import Foundation
+
+struct TweetDataModel {
+    var user: String
+    var tweet: String
+}
