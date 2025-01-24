0f65e64651d376970934e23d08de1fe8b173bc98
satoshi2110 <zappawater@gmail.com>
2025-01-24 21:45:34 +0900

    xibの追加

---
+++ DemoTwitterApp4/Cell.swift
@@ -0,0 +1,27 @@
+//
+//  Cell.swift
+//  DemoTwitterApp4
+//
+//  Created by N S on 2025/01/24.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    @IBOutlet weak var tweet: UILabel!
+    @IBOutlet weak var user: UILabel!
+    @IBOutlet weak var tweetView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
